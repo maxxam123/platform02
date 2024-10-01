a9ef68b80449b33748aa3b0d2b117b4a8301a538
max <81824845+maxxam123@users.noreply.github.com>
2024-10-01 06:43:33 -0700

    Update 02-cluster.tf

--- templates/terraform/02-cluster.tf
+++ templates/terraform/02-cluster.tf
@@ -1,12 +1,3 @@
-#resource "aws_instance" "ins_nihao" {
-#  ami = "ami-01e444924a2233b07"
-#  instance_type = "t2.micro"
-#  subnet_id = "subnet-00e39a3f15e5c70f7"
-#  tags = {
-#    Name = "Ins nihao"
-#  }
-#}
-
 resource "aws_iam_user" "USER" {
   name = "USER"
 }
