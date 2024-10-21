9c4fbbd9ef7d2749231e86502fa022f89ee94da0
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 08:58:53 -0700

    Update 01_lambda_del.sh

--- 04_scripts/01_lambda_del.sh
+++ 04_scripts/01_lambda_del.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
   NAME=$( sed -n 1p 03_trigger/01_lambda_del )
   PROVIDER=$( sed -n 2p 01_infra/lambda/$NAME/values )
-  echo NAME
-  echo $NAME
+
+  
   
   echo $NAME > platf04/03_trigger/test
   
-  mkdir -p platf04/.github/workflows
-  # cp 02_tmp/02_pipeline/01_lambda/main_delete.yaml .
-  cp 02_tmp/02_pipeline/01_lambda/$PROVIDER/main_delete.yaml .
-  sed -i -e "s/NAME/$NAME/g" main_delete.yaml
-  cp main_delete.yaml platf04/.github/workflows/
+  # cp 02_tmp/02_pipeline/01_lambda/$PROVIDER/main_delete.yaml .
+  # sed -i -e "s/NAME/$NAME/g" main_delete.yaml
+  # cp main_delete.yaml platf04/.github/workflows/
+  # sed -i -e "s/NAME/$NAME/g" 02_tmp/02_pipeline/01_lambda/$PROVIDER/main_delete.yaml > platf04/.github/workflows/
