7f3107d5d747ac0bc94957291f4e1a828ffb54be
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 08:24:55 -0700

    Update vpc.sh

--- 04_scripts/vpc.sh
+++ 04_scripts/vpc.sh
@@ -2,20 +2,21 @@
 
   git clone https://github.com/maxxam123/platf04.git
   
-  START=$( sed -n 1p 03_trigger/02_vpc )
-  NAME=$( sed -n 1p 01_infra/vpc/$START/values )
-  COUNT=$( sed -n 3p 01_infra/vpc/$START/values )
-  PROVIDER=$( sed -n 2p 01_infra/vpc/$START/values )
+  NAME=$( sed -n 1p 03_trigger/02_vpc )
+  PROVIDER=$( sed -n 2p 01_infra/vpc/$NAME/values )
+  
   mkdir -p platf04/01_infra/vpc/$NAME
   
-  cp 02_tmp/02_pipeline/02_vpc/$PROVIDER/main.yaml $NAME.yaml
-  sed -i -e "s/NAME/$NAME/g" $NAME.yaml
-  cp $NAME.yaml platf04/.github/workflows/
+  # cp 02_tmp/02_pipeline/02_vpc/$PROVIDER/main.yaml $NAME.yaml
+  # sed -i -e "s/NAME/$NAME/g" $NAME.yaml
+  # cp $NAME.yaml platf04/.github/workflows/
+  sed -i -e "s/NAME/$NAME/g" 02_tmp/02_pipeline/02_vpc/$PROVIDER/main.yaml > platf04/.github/workflows/
   
-  cp 02_tmp/01_terraform/01_providers/$PROVIDER/02_provider.tf .
-  sed -i -e "s/BUCKET/$NAME/g" 02_provider.tf
+  # cp 02_tmp/01_terraform/01_providers/$PROVIDER/02_provider.tf .
+  # sed -i -e "s/BUCKET/$NAME/g" 02_provider.tf
+  # cp 02_provider.tf platf04/01_infra/vpc/$NAME/02_provider.tf
+  sed -i -e "s/BUCKET/$NAME/g" 02_tmp/01_terraform/01_providers/$PROVIDER/02_provider.tf > platf04/01_infra/vpc/$NAME/02_provider.tf
   
-  cp 01_infra/vpc/$START/terraform.tfvars .
-  
-  cp terraform.tfvars platf04/01_infra/vpc/$NAME/terraform.tfvars
-  cp 02_provider.tf platf04/01_infra/vpc/$NAME/02_provider.tf
+  # cp 01_infra/vpc/$START/terraform.tfvars .
+  # cp terraform.tfvars platf04/01_infra/vpc/$NAME/terraform.tfvars
+  cp 01_infra/vpc/$START/terraform.tfvars platf04/01_infra/vpc/$NAME/terraform.tfvars
