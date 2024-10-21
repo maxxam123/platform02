371e645152362a12ed9a7c7b5a74f7916847d86a
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 10:20:12 -0700

    Update 03_subnet_pub_del.sh

--- 04_scripts/03_subnet_pub_del.sh
+++ 04_scripts/03_subnet_pub_del.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+  NAME=$( sed -n 1p 03_trigger/03_subnet_pub_del )
+  PROVIDER=$( sed -n 2p 01_infra/03_subnet_pub/$NAME/values )
+
+  d_pipeline="platf04/.github/workflows"
+  pipeline="02_tmp/02_pipeline/03_subnet_pub"
+  
+  echo $NAME > platf04/03_trigger/test
+  
+  sed -e "s/NAME/$NAME/g" $pipeline/$PROVIDER/main_delete.yaml > $d_pipeline/main_delete.yaml
+
+  # cp 02_tmp/02_pipeline/01_lambda/$PROVIDER/main_delete.yaml .
+  # sed -i -e "s/NAME/$NAME/g" main_delete.yaml
+  # cp main_delete.yaml platf04/.github/workflows/
