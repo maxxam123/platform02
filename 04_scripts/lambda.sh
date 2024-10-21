c4422d5a378cdab790cbe3d6b747bfc37f144492
max <81824845+maxxam123@users.noreply.github.com>
2024-10-21 07:35:54 -0700

    Update lambda.sh

--- 04_scripts/lambda.sh
+++ 04_scripts/lambda.sh
@@ -6,20 +6,23 @@
   NAME=$( sed -n 1p 01_infra/lambda/$START/values )
   PROVIDER=$( sed -n 2p 01_infra/lambda/$START/values )
   
-  cp 02_tmp/02_pipeline/01_lambda/$PROVIDER/main.yaml $NAME.yaml
-  sed -i -e "s/NAME/$NAME/g" $NAME.yaml
-  cp $NAME.yaml platf04/.github/workflows/
+  # cp 02_tmp/02_pipeline/01_lambda/$PROVIDER/main.yaml $NAME.yaml
+  # sed -i -e "s/NAME/$NAME/g" $NAME.yaml
+  # cp $NAME.yaml platf04/.github/workflows/
+
+  sed -i -e "s/NAME/$NAME/g" 02_tmp/02_pipeline/01_lambda/$PROVIDER/main.yaml > platf04/.github/workflows/main.yaml
   
   mkdir -p platf04/01_infra/lambda/$NAME
   
-  cp 01_infra/lambda/$START/index.js .
-  cp index.js platf04/01_infra/lambda/$NAME/index.js
+  cp 01_infra/lambda/$START/index.js platf04/01_infra/lambda/$NAME/index.js
+  # cp index.js platf04/01_infra/lambda/$NAME/index.js
   
   
-  cp 02_tmp/01_terraform/01_providers/$PROVIDER/02_provider.tf .
+  # cp 02_tmp/01_terraform/01_providers/$PROVIDER/02_provider.tf .
+  # cp 02_provider.tf platf04/01_infra/lambda/$NAME/02_provider.tf
   
-  sed -i -e "s/BUCKET/$NAME/g" 02_provider.tf
+  sed -i -e "s/BUCKET/$NAME/g" 02_tmp/01_terraform/01_providers/$PROVIDER/02_provider.tf > platf04/01_infra/lambda/$NAME/02_provider.tf 
   
-  cp 01_infra/lambda/$START/terraform.tfvars .
-  cp terraform.tfvars platf04/01_infra/lambda/$NAME/terraform.tfvars
-  cp 02_provider.tf platf04/01_infra/lambda/$NAME/02_provider.tf
+  # cp 01_infra/lambda/$START/terraform.tfvars .
+  # cp terraform.tfvars platf04/01_infra/lambda/$NAME/terraform.tfvars
+  cp 01_infra/lambda/$START/terraform.tfvars platf04/01_infra/lambda/$NAME/terraform.tfvars
