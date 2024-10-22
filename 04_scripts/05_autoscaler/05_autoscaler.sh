9188c8e94803b054a5cb7cc2fc619f5241b1d64a
max <81824845+maxxam123@users.noreply.github.com>
2024-10-22 02:49:37 -0700

    Update 05_autoscaler.sh

--- 04_scripts/05_autoscaler/05_autoscaler.sh
+++ 04_scripts/05_autoscaler/05_autoscaler.sh
@@ -6,14 +6,14 @@
   PROVIDER=$( sed -n 2p 01_infra/05_autoscaler/$NAME/values )
 
   d_pipeline="platf04/.github/workflows"
-  d_infra="platf04/01_infra/autoscaler"
+  d_infra="platf04/01_infra/05_autoscaler"
   infra="01_infra/05_autoscaler"
   pipeline="02_tmp/02_pipeline/05_autoscaler"
   provider="02_tmp/01_terraform/01_providers"
   mkdir -p $d_pipeline
   mkdir -p $d_infra/$NAME
   
-  sed -e "s/NAME/$NAME/g" $pipeline/$PROVIDER/main.yaml > $d_pipeline/$NAME.yaml
+  sed -e "s/NAME/$NAME/g" $pipeline/$PROVIDER/main.yaml > $d_pipeline/05_$NAME.yaml
   sed -e "s/BUCKET/$NAME/g" $provider/$PROVIDER/02_provider.tf > $d_infra/$NAME/02_provider.tf
   
   cp $infra/$NAME/terraform.tfvars $d_infra/$NAME/terraform.tfvars
