5ff63d93e3cb67699923c26b4e31110625b65ef7
max <81824845+maxxam123@users.noreply.github.com>
2024-10-23 03:03:46 -0700

    Update 02_eks.sh

--- 04_scripts/02_eks.sh
+++ 04_scripts/02_eks.sh
@@ -2,10 +2,10 @@
 
   SERVICE=$( sed -n 3p 03_trigger/01_lambda )
   
-  NGINX=$( sed -n 5p 03_trigger/01_lambda )
-  CERTMANAGER=$( sed -n 6p 03_trigger/01_lambda )
-  ESO=$( sed -n 7p 03_trigger/01_lambda )
-  PROMETHEUS=$( sed -n 8p 03_trigger/01_lambda )
+  NGINX=$( sed -n 5p 03_trigger/01_lambda | awk '{print $2}' )
+  CERTMANAGER=$( sed -n 6p 03_trigger/01_lambda | awk '{print $2}' )
+  ESO=$( sed -n 7p 03_trigger/01_lambda | awk '{print $2}' )
+  PROMETHEUS=$( sed -n 8p 03_trigger/01_lambda | awk '{print $2}' )
   
   d_helm="platf04/05_helm"
   bootstrap="02_tmp/03_git/01_bootstrap"
