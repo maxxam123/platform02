f1d6d8f8f84f207902012306f05695014620ec5e
max <81824845+maxxam123@users.noreply.github.com>
2024-10-24 05:57:16 -0700

    Update 02_eks_del.sh

--- 04_scripts/02_eks_del.sh
+++ 04_scripts/02_eks_del.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-  NAM=$( sed -n 1p 03_trigger/01_lambda_del )
-  PRO=$( sed -n 2p 03_trigger/01_lambda_del )
-  SER=$( sed -n 3p 03_trigger/01_lambda_del )
+  # NAM=$( sed -n 1p 03_trigger/01_lambda_del )
+  # PRO=$( sed -n 2p 03_trigger/01_lambda_del )
+  # SER=$( sed -n 3p 03_trigger/01_lambda_del )
 
-  AUT=$( sed -n 5p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
-  EFS=$( sed -n 6p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
-  SEC=$( sed -n 7p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
-  NGI=$( sed -n 8p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
-  CER=$( sed -n 9p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
+  NAME=$( sed -n 1p 03_trigger/01_lambda_del )
+  PROVIDER=$( sed -n 2p 03_trigger/01_lambda_del )
+  PROVIDER=$( sed -n 3p 03_trigger/01_lambda_del )
+
+  AUT=$( sed -n 5p 01_infra/10_eks/$PROVIDER/$NAME/values.yaml | awk '{print $2}' )
+  EFS=$( sed -n 6p 01_infra/10_eks/$PROVIDER/$NAME/values.yaml | awk '{print $2}' )
+  SEC=$( sed -n 7p 01_infra/10_eks/$PROVIDER/$NAME/values.yaml | awk '{print $2}' )
+  NGI=$( sed -n 8p 01_infra/10_eks/$PROVIDER/$NAME/values.yaml | awk '{print $2}' )
+  CER=$( sed -n 9p 01_infra/10_eks/$PROVIDER/$NAME/values.yaml | awk '{print $2}' )
 
   # # ARN=$( sed -n 12p 01_infra/06_eks/$NAME/scripts/values.yaml | awk '{print $2}' )
 
   echo LS
   ls -la
   ls 
-  touch platf04/01_infra/$SER/$PRO/$NAM/scripts/script_del.sh
-  ls platf04/01_infra/$SER/$PRO/$NAM/scripts
+  touch platf04/01_infra/$SERVICE/$PROVIDER/$NAME/scripts/script_del.sh
+  ls platf04/01_infra/$SERVICE/$PROVIDER/$NAME/scripts
   # if [ $AUT ]; then
   #   echo "cp ../../../../02_tmp/$SER/$PRO/08_autoscaler.tf ." >> platf04/01_infra/$SER/$PRO/$NAM/scripts/script_del.sh
   # fi
