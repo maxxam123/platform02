c1b3de40c3df7cecbcdbd9f98a36c0e320b81048
max <81824845+maxxam123@users.noreply.github.com>
2024-10-24 05:02:06 -0700

    Create 02_eks_del.sh

---
+++ 04_scripts/02_eks_del.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+  NAM=$( sed -n 1p 03_trigger/01_lambda )
+  PRO=$( sed -n 2p 03_trigger/01_lambda )
+  SER=$( sed -n 3p 03_trigger/01_lambda )
+
+  AUT=$( sed -n 5p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
+  EFS=$( sed -n 6p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
+  SEC=$( sed -n 7p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
+  NGI=$( sed -n 8p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
+  CER=$( sed -n 9p 01_infra/10_eks/$PRO/$NAM/values.yaml | awk '{print $2}' )
+
+  # # ARN=$( sed -n 12p 01_infra/06_eks/$NAME/scripts/values.yaml | awk '{print $2}' )
+
+  touch platf04/01_infra/$SER/$PRO/$NAM/scripts/script_del.sh
+  if [ $AUT ]; then
+    echo "cp ../../../../02_tmp/$SER/$PRO/08_autoscaler.tf ." >> platf04/01_infra/$SER/$PRO/$NAM/scripts/script_del.sh
+  fi
+
+  if [ $EFS ]; then
+    echo "cp ../../../../02_tmp/$SER/$PRO/09_efs.tf ." >> platf04/01_infra/$SER/$PRO/$NAM/scripts/script_del.sh
+  fi
+
+  if [ $SEC ]; then
+    echo "cp ../../../../02_tmp/$SER/$PRO/10_secdriver.tf ." >> platf04/01_infra/$SER/$PRO/$NAM/scripts/script_del.sh
+  fi
+
+  cat main.yaml
